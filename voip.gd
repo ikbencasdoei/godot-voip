1837cae571281707a2ffc1678c420ecd958020f8
cbarsugman <casbrugman@hotmail.nl>
2020-05-08 20:12:48 +0200

    Few tweaks
    Fixed a bug where the script was trying to send record data of size 0. Also renamed an repurposed variable MIN_PACKET_SIZE to MIN_PACKET_LENGTH.

--- voip.gd
+++ voip.gd
@@ -1,14 +1,14 @@
 extends AudioStreamPlayer
 
-const MIN_PACKET_SIZE = 50000
+const MIN_PACKET_LENGTH = 1
 
 var mic : AudioEffectRecord
 var record
 var recording = false
+var time_elapsed = 0
 
 func _ready():
 	mic = AudioServer.get_bus_effect(AudioServer.get_bus_index("Record"), 0)
-	mic.set_recording_active(true)
 
 remote func _play(id, audioPacket, format, mix_rate, stereo):
 	get_node("/root/Control/Log").text += "received audio from player with id: %s\n" % id
@@ -20,22 +20,22 @@ remote func _play(id, audioPacket, format, mix_rate, stereo):
 	audioStream.set_stereo(stereo)
 	stream = audioStream
 	play()
-
-var time_elapsed = 0
+	
 func _process(delta: float) -> void:
 	if recording:
 		if mic.is_recording_active():
-			if time_elapsed > 1:
+			if time_elapsed >= MIN_PACKET_LENGTH:
 				mic.set_recording_active(false)
 				record = mic.get_recording()
 				rpc_unreliable("_play",get_tree().get_network_unique_id(), record.get_data(), record.get_format(), record.get_mix_rate(), record.is_stereo())
 				get_node("/root/Control/Log").text += "send recording of size %s\n" % record.get_data().size()
 				mic.set_recording_active(true)
 				time_elapsed = 0
+				
+			time_elapsed += delta
 		else:
 			mic.set_recording_active(true)
 	else:
 		mic.set_recording_active(false)
-
-	time_elapsed += delta
+		
 
