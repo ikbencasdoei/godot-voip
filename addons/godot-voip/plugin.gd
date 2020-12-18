b163ce5bf2153e72c1d046da18d714d49f151f10
cbarsugman <casbrugman@hotmail.nl>
2020-12-18 23:46:38 +0100

    Fixed plugin.gd

--- addons/godot-voip/plugin.gd
+++ addons/godot-voip/plugin.gd
@@ -5,4 +5,4 @@ func _enter_tree() -> void:
 	add_custom_type("VoipInstance", "Node", preload("res://addons/godot-voip/scripts/voip_instance.gd"), preload("res://addons/godot-voip/icons/VoipInstance.svg"))
 
 func _exit_tree() -> void:
-	pass
+	remove_custom_type("VoipInstance")
