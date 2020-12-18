020ff0fa0a7f56fdf119651512d82ce8d97976ed
cbarsugman <casbrugman@hotmail.nl>
2020-12-18 23:25:44 +0100

    Cleaner?

--- addons/godot-voip/scripts/voip_microphone.gd
+++ addons/godot-voip/scripts/voip_microphone.gd
@@ -7,14 +7,12 @@ func _ready() -> void:
 		current_number += 1
 
 	var bus_name = "VoipMicrophoneRecorder" + str(current_number)
-
 	var idx = AudioServer.bus_count
 
 	AudioServer.add_bus(idx)
 	AudioServer.set_bus_name(idx, bus_name)
 
 	AudioServer.add_bus_effect(idx, AudioEffectRecord.new())
-
 	AudioServer.set_bus_mute(idx, true)
 
 	bus = bus_name
