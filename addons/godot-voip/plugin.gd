11eb13eff411a54c42687562bb38f40eb647459d
cbarsugman <casbrugman@hotmail.nl>
2020-12-18 21:31:08 +0100

    Updated paths

--- addons/godot-voip/plugin.gd
+++ addons/godot-voip/plugin.gd
@@ -2,7 +2,7 @@ tool
 extends EditorPlugin
 
 func _enter_tree() -> void:
-	add_custom_type("VoipInstance", "Node", preload("res://addons/godot-voip/voip/voip_instance.gd"), preload("res://addons/godot-voip/icon.svg"))
+	add_custom_type("VoipInstance", "Node", preload("res://addons/godot-voip/scripts/voip_instance.gd"), preload("res://addons/godot-voip/icons/VoipInstance.svg"))
 
 func _exit_tree() -> void:
 	pass
