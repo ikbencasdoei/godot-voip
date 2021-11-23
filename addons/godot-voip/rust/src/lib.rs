6dd92f276ea42a210d0356ae336bfcd4f043e9e9
Cas Brugman <casbrugman@hotmail.nl>
2021-11-23 20:27:37 +0100

    Begin implementation
    Setting and getting roperties done

--- addons/godot-voip/rust/src/lib.rs
+++ addons/godot-voip/rust/src/lib.rs
@@ -1,6 +1,9 @@
 use gdnative::prelude::*;
 
+mod voice_instance;
+
 fn init(handle: InitHandle) {
+    handle.add_class::<voice_instance::NativeVoiceInstance>();
 }
 
 godot_init!(init);
---
+++ addons/godot-voip/rust/src/voice_instance.rs
@@ -0,0 +1,110 @@
+use gdnative::{nativescript::property::*, prelude::*};
+
+#[derive(NativeClass)]
+#[inherit(Node)]
+#[register_with(Self::register_class)]
+pub struct NativeVoiceInstance {
+    custom_voice_audio_stream_player: NodePath,
+    recording: bool,
+    listen: bool,
+    input_threshold: f32,
+}
+
+#[methods]
+impl NativeVoiceInstance {
+    fn new(_owner: &Node) -> Self {
+        NativeVoiceInstance {
+            custom_voice_audio_stream_player: NodePath::from_str(""),
+            recording: false,
+            listen: false,
+            input_threshold: 0.005,
+        }
+    }
+
+    fn register_class(builder: &ClassBuilder<Self>) {
+        builder.add_signal(Signal {
+            name: "received_voice_data",
+            args: &[],
+        });
+        builder.add_signal(Signal {
+            name: "sent_voice_data",
+            args: &[],
+        });
+        builder.add_signal(Signal {
+            name: "created_instance",
+            args: &[],
+        });
+        builder.add_signal(Signal {
+            name: "removed_instance",
+            args: &[],
+        });
+
+        builder
+            .add_property::<NodePath>("custom_voice_audio_stream_player")
+            .with_ref_getter(NativeVoiceInstance::get_custom_voice_audio_stream_player)
+            .with_setter(NativeVoiceInstance::set_custom_voice_audio_stream_player)
+            .with_default(NodePath::from_str(""))
+            .done();
+        builder
+            .add_property::<bool>("recording")
+            .with_ref_getter(NativeVoiceInstance::get_recording)
+            .with_setter(NativeVoiceInstance::set_recording)
+            .with_default(false)
+            .done();
+        builder
+            .add_property::<bool>("listen")
+            .with_ref_getter(NativeVoiceInstance::get_listen)
+            .with_setter(NativeVoiceInstance::set_listen)
+            .with_default(false)
+            .done();
+        builder
+            .add_property::<f32>("input_threshold")
+            .with_ref_getter(NativeVoiceInstance::get_input_threshold)
+            .with_setter(NativeVoiceInstance::set_input_threshold)
+            .with_default(0.005)
+            .with_hint(FloatHint::Range(RangeHint {
+                min: 0.0,
+                max: 1.0,
+                step: None,
+                or_greater: false,
+                or_lesser: false,
+            }))
+            .done();
+    }
+
+    fn get_custom_voice_audio_stream_player(&self, _owner: TRef<Node>) -> &NodePath {
+        &self.custom_voice_audio_stream_player
+    }
+
+    fn set_custom_voice_audio_stream_player(&mut self, _owner: TRef<Node>, value: NodePath) {
+        self.custom_voice_audio_stream_player = value;
+    }
+
+    fn get_recording(&self, _owner: TRef<Node>) -> &bool {
+        &self.recording
+    }
+
+    fn set_recording(&mut self, _owner: TRef<Node>, value: bool) {
+        self.recording = value;
+    }
+
+    fn get_listen(&self, _owner: TRef<Node>) -> &bool {
+        &self.listen
+    }
+
+    fn set_listen(&mut self, _owner: TRef<Node>, value: bool) {
+        self.listen = value;
+    }
+
+    fn get_input_threshold(&self, _owner: TRef<Node>) -> &f32 {
+        &self.input_threshold
+    }
+
+    fn set_input_threshold(&mut self, _owner: TRef<Node>, value: f32) {
+        self.input_threshold = value;
+    }
+
+    #[export]
+    fn _ready(&self, _owner: &Node) {
+    }
+}
