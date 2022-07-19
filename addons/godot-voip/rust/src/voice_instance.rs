f062e500d4649c79cfe1a341c89e9a58d29bb093
Cas Brugman <casbrugman@hotmail.nl>
2022-07-19 20:30:08 +0200

    Some descriptions

--- addons/godot-voip/rust/src/voice_instance.rs
+++ addons/godot-voip/rust/src/voice_instance.rs
@@ -42,6 +42,7 @@ impl NativeVoiceInstance {
         let generator = AudioStreamGenerator::new();
         generator.set_buffer_length(0.1f64);
 
+        //check what kind of audioplayer the given node is (if at all)
         if let Some(voice) = voice.cast::<AudioStreamPlayer>() {
             voice.set_stream(generator);
             self.playback = Some(
@@ -203,6 +204,7 @@ impl NativeVoiceInstance {
                 playback.get_frames_available(),
                 self.receive_buffer.len() as i64,
             ) {
+                //convert samples back to stereo
                 playback.push_frame(Vector2::new(
                     self.receive_buffer.get(0),
                     self.receive_buffer.get(0),
@@ -232,7 +234,7 @@ impl NativeVoiceInstance {
             }
         });
         audio_server.add_bus_effect(record_bus_index, AudioEffectCapture::new(), 0);
-        audio_server.set_bus_mute(record_bus_index, true);
+        audio_server.set_bus_mute(record_bus_index, true); //mute because this audiobus is for capturing microhphone audio
         self.effect_capture = Some(
             audio_server
                 .get_bus_effect(record_bus_index, 0)
@@ -258,6 +260,7 @@ impl NativeVoiceInstance {
             let effect_capture = unsafe { self.effect_capture.as_ref().unwrap().assume_safe() };
 
             if effect_capture.get_frames_available() > 0 {
+                //if recording just started: clear buffer to prevent sending of unintended recorded sound.
                 if !self.prev_frame_recording {
                     effect_capture.clear_buffer();
                 }
@@ -267,10 +270,9 @@ impl NativeVoiceInstance {
                     .read()
                     .to_vec();
 
+                //convert samples to mono + get amplitude
                 let mut vec_mono_data: Vec<f32> = Vec::with_capacity(vec_stereo_data.len());
-
                 let mut max_value = 0f32;
-
                 for vector in vec_stereo_data.iter() {
                     let value = (vector.x + vector.y) / 2.0;
 
@@ -278,18 +280,18 @@ impl NativeVoiceInstance {
                     vec_mono_data.push(value);
                 }
 
+                // continue if amplitude is high enough
                 if max_value < self.input_threshold {
                     return;
                 }
 
                 let mono_data = PoolArray::from_vec(vec_mono_data);
 
+                //send voice data
                 if self.listen {
                     self.speak(owner, mono_data.clone());
                 }
-
                 owner.rpc_unreliable("speak", &[mono_data.to_variant()]);
-
                 owner.emit_signal("sent_voice_data", &[mono_data.to_variant()]);
             }
         }
