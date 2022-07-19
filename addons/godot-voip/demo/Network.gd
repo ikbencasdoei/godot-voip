8f5d1fdf393b8549a95249e081e17859d5853c60
Cas Brugman <casbrugman@hotmail.nl>
2022-07-19 21:13:48 +0200

    Added ability to switch between enet and websocket to network

--- addons/godot-voip/demo/Network.gd
+++ addons/godot-voip/demo/Network.gd
@@ -4,28 +4,56 @@ class_name Network
 var server_port := 3000
 var server_ip := "127.0.0.1"
 
+enum ClientType {ENET, WEBSOCKET}
+export(ClientType) var client_type
+
 func start_client() -> int:
-	var peer := WebSocketClient.new()
+	if client_type == ClientType.ENET:
+		var peer := NetworkedMultiplayerENet.new()
 
-	var err := peer.connect_to_url("ws://%s:%s" % [server_ip, server_port], PoolStringArray(), true)
-	if err != OK:
-		return err
+		var err = peer.create_client(server_ip, server_port)
+		if err != OK:
+			return err
 
-	get_tree().set_network_peer(peer)
+		get_tree().set_network_peer(peer)
 
-	return OK
+		return OK
+	elif client_type == ClientType.WEBSOCKET:
+		var peer := WebSocketClient.new()
+
+		var err = peer.connect_to_url("ws://%s:%s" % [server_ip, server_port], PoolStringArray(), true)
+		if err != OK:
+			return err
+
+		get_tree().set_network_peer(peer)
+
+		return OK
+	return FAILED
 
 func start_server() -> int:
-	var peer := WebSocketServer.new()
+	if client_type == ClientType.ENET:
+		var peer := NetworkedMultiplayerENet.new()
 
-	var err := peer.listen(server_port, PoolStringArray(), true)
+		var err := peer.create_server(server_port)
 
-	if err != OK:
-		return err
+		if err != OK:
+			return err
 
-	get_tree().set_network_peer(peer)
+		get_tree().set_network_peer(peer)
 
-	return OK
+		return OK
+	elif client_type == ClientType.WEBSOCKET:
+		var peer := WebSocketServer.new()
+
+		var err := peer.listen(server_port, PoolStringArray(), true)
+
+		if err != OK:
+			return err
+
+		get_tree().set_network_peer(peer)
+
+		return OK
+	return FAILED
 
 func stop() -> void:
 	if get_tree().network_peer != null:
@@ -33,4 +61,6 @@ func stop() -> void:
 			get_tree().network_peer.disconnect_from_host()
 		elif get_tree().network_peer is WebSocketServer:
 			get_tree().network_peer.stop()
+		elif get_tree().network_peer is NetworkedMultiplayerENet:
+			get_tree().network_peer.close_connection()
 
