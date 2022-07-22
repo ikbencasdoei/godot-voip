6ae2c203ec3b4524627d62387bbfb720a0ce0849
Cas Brugman <casbrugman@hotmail.nl>
2022-07-22 20:58:10 +0200

    Unique prefix

--- addons/godot-voip/gdnative/src/lib.rs
+++ addons/godot-voip/gdnative/src/lib.rs
@@ -1,4 +1,10 @@
-use gdnative::prelude::*;
+use gdnative::{
+    init::{
+        godot_gdnative_init, godot_gdnative_terminate, godot_nativescript_init, InitializeInfo,
+        TerminateInfo,
+    },
+    prelude::*,
+};
 
 mod voice_instance;
 
@@ -6,4 +12,9 @@ fn init(handle: InitHandle) {
     handle.add_class::<voice_instance::NativeVoiceInstance>();
 }
 
-godot_init!(init);
+fn godot_gdnative_init_empty(_options: &InitializeInfo) {}
+fn godot_gdnative_terminate_empty(_term_info: &TerminateInfo) {}
+
+godot_gdnative_init!(godot_gdnative_init_empty as godot_voip_gdnative_init);
+godot_nativescript_init!(init as godot_voip_nativescript_init);
+godot_gdnative_terminate!(godot_gdnative_terminate_empty as godot_voip_gdnative_terminate);
